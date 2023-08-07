04d0b5e37d0512cfccfbb7fac5c0c748238f7f67
KolenkoGri <130161094+KolenkoGri@users.noreply.github.com>
2023-08-07 11:32:47 +0400

    Add files via upload

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import mark from './img/mark.jpg';
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img src={mark} />
+    </>
+);
+
+export default Module;
